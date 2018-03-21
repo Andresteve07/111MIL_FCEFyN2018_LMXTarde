1548278cc9bc1a7b3006c5de5a27ef7516015545
Santiago Salamandri <santiagosalamandri@gmail.com>
2018-03-21 16:02:45 -0300

    soluciones

--- QuizArreglos/src/main/java/quizarreglos/Colecciones.java
+++ QuizArreglos/src/main/java/quizarreglos/Colecciones.java
@@ -11,16 +11,51 @@ package quizarreglos;
  */
 public class Colecciones {
     public static char[] apilar(char[] palabraOriginal,char letra){
-        return null;
+
+    char[] apilado=new char[(palabraOriginal.length)+1];
+    int i=0;
+    
+    for(i=0;i<palabraOriginal.length;i++){
+        apilado[i]=palabraOriginal[i];
+    }
+     apilado[i]=letra;
+    
+    return apilado;
+    
     }
     public static char[] desapilar(char[] palabraOriginal){
-        return null;
+
+        char[] desapilado=new char[(palabraOriginal.length)-1];
+    int i=0;
+    
+    for(i=0;i<palabraOriginal.length-1;i++){
+        desapilado[i]=palabraOriginal[i];
+    }
+
+        return desapilado;
     }
     public static char[] encolar(char[] palabraOriginal,char letra){
-        return null;
+        
+        char[] encolado=new char[(palabraOriginal.length)+1];
+        int i=0;
+    
+    encolado[0]=letra;
+    
+    for(i=0;i<palabraOriginal.length;i++){
+        encolado[i+1]=palabraOriginal[i];
+    }
+
+        return encolado;
     }
     public static char[] desencolar(char[] palabraOriginal){
-        return null;
+        char[] desencolaOK={'b','c','d'};
+        char[] desencolado=new char[(palabraOriginal.length)-1];
+int i=0;
+
+         for(i=0;i<palabraOriginal.length-1;i++){
+        desencolado[i]=palabraOriginal[i+1];
+    }
+        return desencolado;
     }
     
 }
